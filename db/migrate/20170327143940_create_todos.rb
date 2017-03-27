66e79cb18a0be63accfdeb8e2d145bcfac80ec7a
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-03-27 23:40:02 +0900

    add test model

---
+++ db/migrate/20170327143940_create_todos.rb
@@ -0,0 +1,10 @@
+class CreateTodos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :todos do |t|
+      t.string :name
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
